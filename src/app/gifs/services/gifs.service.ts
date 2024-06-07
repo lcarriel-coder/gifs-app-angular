9a88e68c8bb908432857dbbe69e1bcc9ccb0344f
Luis Carriel <lcarriel@imaget.com>
2024-06-07 17:52:13 -0500

    data

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,19 +1,42 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
-@Injectable({providedIn: 'root'})
+
+@Injectable({ providedIn: 'root' })
 export class GifsService {
 
-  private _tagsHistory:string[] =[];
+  public gifList: Gif[] = [];
+  private _tagsHistory: string[] = [];
+  private apiKey: string = 'Xr3SgYjfPeCGN52JMN7hZTceCOzD9o9s';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
+  constructor(private http: HttpClient) {
 
-  constructor() { }
+  }
 
-  get tagsHistory(){
+  get tagsHistory() {
     return [...this._tagsHistory];
   }
 
 
-  public searchTag(tag:string):void{
-    this._tagsHistory.unshift( tag );
+  searchTag(tag: string) {
+    this._tagsHistory.unshift(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', tag);
+
+
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search`, { params })
+      .subscribe(resp => {
+        this.gifList = resp.data;
+      }
+      )
+
+
+
+
   }
 }

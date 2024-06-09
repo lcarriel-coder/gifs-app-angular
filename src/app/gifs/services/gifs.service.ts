1ba794e1982c5d44f80c095f69799bbe935d613d
Luis Carriel <imagetech.developer11@outlook.com>
2024-06-09 16:39:46 -0500

    terminandoo gifs

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -12,16 +12,48 @@ export class GifsService {
   private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
   constructor(private http: HttpClient) {
-
+    this.loadLocalStorage();
   }
 
   get tagsHistory() {
     return [...this._tagsHistory];
   }
 
+  private organizeHistory(tag:string){
+    tag= tag.toLowerCase();
+
+    if(this._tagsHistory.includes( tag )){
+      this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag !== tag );
+    }
+
+    this._tagsHistory.unshift( tag );
+    this._tagsHistory = this.tagsHistory.splice(0,10);
+    this.saveLocalStorage();
+
+
+  }
+
+  private saveLocalStorage() :void{
+    localStorage.setItem('history', JSON.stringify(this.tagsHistory));
+  }
+
+  private loadLocalStorage () :void {
+    if( !localStorage.getItem('history')) return;
+
+    this._tagsHistory =JSON.parse(localStorage.getItem('history')!) ;
+
+    if(this.tagsHistory.length === 0)return;
+    if(this.tagsHistory.length > 0){
+      this.searchTag(this._tagsHistory[0]);
+    }
+
+
+  }
 
   searchTag(tag: string) {
-    this._tagsHistory.unshift(tag);
+    if(tag.length === 0) return;
+    this.organizeHistory(tag);
+    //this._tagsHistory.unshift(tag);
 
     const params = new HttpParams()
       .set('api_key', this.apiKey)
